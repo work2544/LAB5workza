f5cb6d07d4c7d658b88120f36355fb03db1b4e21
work2544 <work21_123@hotmail.com>
2020-12-15 16:00:01 +0700

    update5_1&5_2

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int inputcase=1,evencount=0,oddcount=0;
+    while(inputcase!=0)
+    {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> inputcase;
+    if(inputcase==0)
+    break;
+    if(inputcase%2==0)
+    {
+        evencount++;
+    }    
+    else if(inputcase%2!=0)
+    {
+        oddcount++;
+    }
+    
+    }
+    cout << "#Even numbers = " <<evencount<<endl;
+    cout << "#Odd numbers = " << oddcount;
     return 0;
+    
 }
--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,35 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double deg)
+{
+ return deg*M_PI/180;
+}
+double rad2deg(double rad)
+{
+ return rad/M_PI*180;
+}
+double findXComponent(double l1,double l2,double a1,double a2)
+{
+return l1*cos(a1)+l2*cos(a2);
+}
+double findYComponent(double l1,double l2,double a1,double a2)
+{
+return l1*sin(a1)+l2*sin(a2);
+}
+double pythagoras(double x,double y)
+{
+    return sqrt(pow(x,2)+pow(y,2));
+}
+void showResult(double length,double direction)
+{
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+cout<<"Length of the resultant vector = "<<length<<endl;
+cout<<"Direction of the resultant vector (deg) = "<<direction;
+cout<<"\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
